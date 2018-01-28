d403ac909dd8ad7865a13f04303b877e4ca14bb9
Luiz Alberto <luiz.alberto1723@gmail.com>
2018-01-27 22:24:29 -0200

    Adicionando Header
    Header adicionado para facilitar a navegação do usuário pelo app.
    
    Até o momento as opções de inicio e notas foram adicionadas.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app dark>
+    <app-header></app-header>
     <router-view></router-view>
     <snack-bar></snack-bar>
   </v-app>
 </template>
 
 <script>
-import SnackBar from './components/SnackBar';
+import AppHeader from '@/components/AppHeader';
+import SnackBar from '@/components/SnackBar';
 
 export default {
   name: 'App',
   components: {
+    AppHeader,
     SnackBar,
   },
 };
---
+++ src/components/AppHeader/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-menu bottom left>
+            <v-btn icon slot="activator" dark>
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+            <v-list>
+              <v-list-tile v-for="(item, i) in items" :key="i">
+                <v-list-tile-title @click="openPage(item.page)">{{ item.title }}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data() {
+    return {
+      items: [
+        { title: 'Inicio', page: '/' },
+        { title: 'Anotações', page: '/notes' },
+      ],
+    };
+  },
+  methods: {
+    openPage(page) {
+      this.$router.push(page);
+    },
+  },
+};
+</script>
