462f0551ce22c3483eb79ae62588ae5a049a4490
Luiz Alberto <luiz.alberto1723@gmail.com>
2018-01-30 21:27:22 -0200

    Adicionando componente para a navegacao entre as paginas
    Adicionando componente para a navegacao entre as paginas para facilitar o uso do app pelo usuário.

---
+++ src/components/Navigation/BackTo.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout row>
+    <v-flex>
+      <h1>
+        <v-btn icon color="orange" @click="openPage()">
+          <v-icon>arrow_back</v-icon>
+        </v-btn>
+        Perfil
+      </h1>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'BackTo',
+  props: {
+    page: {
+      type: String,
+      default: '/',
+    },
+  },
+  methods: {
+    openPage() {
+      this.$router.push(this.page);
+    },
+  },
+};
+</script>
