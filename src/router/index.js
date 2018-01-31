c9d07e2237692b4b652c4f21814cb31f01cdc0c5
onluiz <luiz.alberto1723@gmail.com>
2018-01-31 13:57:01 -0200

    ajustando layout do header do app. Ajustando rotas.

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,10 @@ const router = new Router({
   routes: [
     {
       path: '/',
+      redirect: '/home',
+    },
+    {
+      path: '/home',
       name: 'HomePage',
       component: HomePage,
     },
