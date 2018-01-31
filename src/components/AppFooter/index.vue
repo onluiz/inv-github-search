5858b8b19b592f7799df7e6ecd6478842bf66f6a
Luiz Alberto <luiz.alberto1723@gmail.com>
2018-01-31 21:01:15 -0200

    Ajustes de logo no app
    Removendo logo em base64 do github
    
    Adicionando logos do github
    
    Ajustando AppHeader para buscar a logo a partir dos assets
    
    Ajustando AppFooter para buscar a data atual mais adequadamente (de forma mais clean)

--- src/components/AppFooter/index.vue
+++ src/components/AppFooter/index.vue
@@ -1,12 +1,17 @@
 <template>
   <v-footer class="pa-3">
     <v-spacer></v-spacer>
-    <div>GitSearch © {{ new Date().getFullYear() }}</div>
+    <div>GitSearch © {{ currentDate }}</div>
   </v-footer>
 </template>
 
 <script>
 export default {
   name: 'AppFooter',
+  data() {
+    return {
+      currentDate: new Date().getFullYear(),
+    };
+  },
 };
 </script>
---
+++ src/components/ExampleButton/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <button>{{ desc }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ExampleButton',
+  props: {
+    desc: {
+      type: String,
+      required: true,
+      default: 'Button Description',
+    },
+  },
+  data() {
+    return {
+      message: 'Hi!',
+    }
+  },
+};
+</script>
+
